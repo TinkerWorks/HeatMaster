f369aeea8f4505875d97aa378df4f9ae79f33126
Alexandru-Sever Horin <alex.sever.h@gmail.com>
2020-04-02 23:28:37 +0300

    Callback propagated from temperature to group

--- HeatMaster/temperature/temperature.py
+++ HeatMaster/temperature/temperature.py
@@ -1,6 +1,9 @@
 class Temperature:
-    def __init__(self):
-        pass
+    def __init__(self, callback):
+        self.setCallback(callback)
 
     def get():
         raise NotImplementedError
+
+    def setCallback(self, callback):
+        self.callback_ = callback
